6f467f41afe7591dc60e8523cf2f26dc13b0e1ca
Jordan Van Walleghem <j.vanwall@gmail.com>
2015-05-08 22:27:10 +0100

    Ajout de la gestion de conflit d'addresse

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,13 +6,21 @@ angular.module('warperLanding', [])
     .controller('HomeController', ['$http', function ($http) {
         var home = this;
         home.i18n = {};
+
         home.submitted = false;
+        home.conflict = false;
 
         home.submitNewsletter = function () {
             $http.post('/newsletter', { email: home.newsletterEmail })
                 .success(function () {
                     home.submitted = true;
                 })
+                .error(function (data, status) {
+                    if (status === 409) {
+                        home.submitted = true;
+                        home.conflict = true;
+                    }
+                });
         };
 
         var locale = navigator.language === 'fr' ? 'fr' : 'en';
